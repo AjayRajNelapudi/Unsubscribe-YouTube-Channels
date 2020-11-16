ba8a73c330e9c3f049bfdf3a8ef6dc1d911d2c0c
ajnelapu <ajnelapu@amazon.com>
2020-11-16 13:21:34 +0530

    Logging added

--- subscribeAll.js
+++ subscribeAll.js
@@ -1,9 +1,17 @@
 function subscribeAll() {
+	console.log('Finding all subscribe buttons...');
 	var subscriptions = document.querySelectorAll('paper-button[aria-label^="Subscribe to"]');
+	console.log('Subscriptions:', subscriptions);
 
 	subscriptions.forEach((subscription) => {
+		var channel = subscription.getAttribute('aria-label').slice(17, -1);
+		console.log('Selected channel:', channel);
+		console.log('Clicking on subscribe button...');
 		subscription.click();
+		console.log('Clicked on subscribe to:', channel);
 	});
 }
 
+console.log('Invoking subscribeAll()');
 subscribeAll();
+console.log('Exited subscribeAll()');
--- unsubscribeAll.js
+++ unsubscribeAll.js
@@ -1,16 +1,18 @@
 function unsubscribeAll() {
 	console.log('Finding all unsubscribe buttons...');
 	var subscriptions = document.querySelectorAll('paper-button[aria-label^="Unsubscribe from"]');
-	console.log('Subscriptions', subscriptions);
+	console.log('Subscriptions:', subscriptions);
 
 	subscriptions.forEach((subscription) => {
+		var channel = subscription.getAttribute('aria-label').slice(17, -1);
+		console.log('Selected channel:', channel);
 		console.log('Clicking on unsubscribe button...');
 		subscription.click();
 		console.log('Finding confirm unsubscribe button...');
 		var unsubscribeButton = document.querySelector('paper-button[aria-label="Unsubscribe"]')
 		console.log('Clicking on confirm unsibscribe button...');
 		unsubscribeButton.click();
-		console.log('Unsubscribed:', subscription);
+		console.log('Clicked confirm unsubscribe from:', channel);
 	});
 }
 
