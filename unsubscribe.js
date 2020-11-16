d933634fb35823dfb2994e04c982acc431f058d2
ajnelapu <ajnelapu@amazon.com>
2020-11-16 12:16:29 +0530

    Inital commit

---
+++ unsubscribe.js
@@ -0,0 +1,11 @@
+function unsubscribeFromAllChannels() {
+	var subscriptions = document.querySelectorAll('paper-button[aria-label^="Unsubscribe from"]');
+
+	subscriptions.forEach((subscription) => {
+		subscription.click();
+		var unsubscribeButton = document.querySelector('paper-button[aria-label="Unsubscribe"]')
+		unsubscribeButton.click();
+	});
+}
+
+unsubscribeFromAllChannels()
