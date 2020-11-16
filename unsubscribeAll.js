9b838745d64b80a1ece3e6619f1c388e108c53cf
ajnelapu <ajnelapu@amazon.com>
2020-11-16 13:08:31 +0530

    logging added

--- unsubscribeAll.js
+++ unsubscribeAll.js
@@ -1,11 +1,19 @@
 function unsubscribeAll() {
+	console.log('Finding all unsubscribe buttons...');
 	var subscriptions = document.querySelectorAll('paper-button[aria-label^="Unsubscribe from"]');
+	console.log('Subscriptions', subscriptions);
 
 	subscriptions.forEach((subscription) => {
+		console.log('Clicking on unsubscribe button...');
 		subscription.click();
+		console.log('Finding confirm unsubscribe button...');
 		var unsubscribeButton = document.querySelector('paper-button[aria-label="Unsubscribe"]')
+		console.log('Clicking on confirm unsibscribe button...');
 		unsubscribeButton.click();
+		console.log('Unsubscribed:', subscription);
 	});
 }
 
+console.log('Invoking unsubscribeAll()');
 unsubscribeAll();
+console.log('Exited unsubscribeAll()');
